364aa2f4eb36f0ebc41c5e5af09451cb4d993809
Ahhhrong <S10247585@connect.np.edu.sg>
2023-11-12 21:41:05 +0800

    Connection with Database

--- Asst/Controllers/QuestionsController.cs
+++ Asst/Controllers/QuestionsController.cs
@@ -2,20 +2,13 @@
 using System.Collections.Generic;
 using Asst.Models;
 using System.Diagnostics;
+using Asst.DAL;
 
 namespace Asst.Controllers
 {
     public class QuestionsController : Controller
     {
-        private Dictionary<string, List<string>> questions = new Dictionary<string, List<string>>
-        {
-            { "chief complaint", new List<string> { "What brings you in for an eye examination today?", "Can you describe the main issue or concern you have with your eyes?" } },
-            { "medical history", new List<string> { "Do you have any existing medical conditions?", "Are you currently taking any medications or eye drops?", "Do you have any known allergies to medications or substances?", "Have you ever had eye surgery or any eye-related procedures in the past?" } },
-            { "symptoms", new List<string> { "Could you describe any symptoms you're experiencing, such as pain, redness, or discomfort?", "When did these symptoms start?", "Are the symptoms constant or intermittent?", "Have you noticed any triggers or patterns related to the symptoms?", "Do you have any associated symptoms?" } },
-            { "pain", new List<string> { "Which eye had the pain?", "What is the severity of the pain between 1 to 10, 10 being the most painful?", "How long have you had the pain?", "How would you describe the pain?", "Are there any other issues you believe could be related to the pain? e.g. redness?", "Have you had this kind of pain before?", "Each time you experience this pain, how long does it last?" } },
-            { "history taking", new List<string> { "Can you describe the patient's chief complaint or reason for the visit?", "Ask about the onset and duration of symptoms.", "Inquire about any relevant medical history or pre-existing conditions.", "Ask about the family history of eye diseases.", "Explore lifestyle factors like smoking, allergies, and occupation.", "Encourage the patient to share any other symptoms or concerns." } }
-            // Add more topics and questions here...
-        };
+        
 
 
 
@@ -32,17 +25,26 @@ namespace Asst.Controllers
             return View(msgList);
         }
 
+
         [HttpPost]
         public ActionResult AskQuestions(string topic)
         {
+            QuestionDAL questionDAL = new QuestionDAL();
+
+            List<QuestionModel> questions = questionDAL.GetQuestions();
+            
             string msgTextList = HttpContext.Session.GetString("MsgList");
             msgTextList += topic + "%&%";
-            if (questions.ContainsKey(topic))
+
+            // Assuming questions is a Dictionary<string, List<string>> where the key is the topic
+            QuestionModel topicQuestions = questions.FirstOrDefault(q => q.Topic.Equals(topic));
+
+            if (topicQuestions != null)
             {
                 msgTextList += "Here are some questions you could ask regarding the topic of " + topic + ":";
-                for (int i = 0; i < questions[topic].Count(); i++)
+                for (int i = 0; i < topicQuestions.Questions.Count; i++)
                 {
-                    msgTextList += "\n"+(i+1).ToString() + ". " + questions[topic][i];
+                    msgTextList += "\n" + (i + 1).ToString() + ". " + topicQuestions.Questions[i];
                 }
                 msgTextList += "%&%";
             }
@@ -50,6 +52,7 @@ namespace Asst.Controllers
             {
                 msgTextList += "Invalid topic or questions not found.%&%";
             }
+
             HttpContext.Session.SetString("MsgList", msgTextList);
             string[] msgArray = msgTextList.Split("%&%");
             List<Message> msgList = new List<Message>();
@@ -58,49 +61,8 @@ namespace Asst.Controllers
             {
                 msgList.Add(new Message { Content = msgArray[i] });
             }
+
             return View(msgList);
         }
     }
 }
-
-/*using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-
-namespace Asst.Controllers
-{
-    public class QuestionsController : Controller
-    {
-        private Dictionary<string, List<string>> questions = new Dictionary<string, List<string>>
-        {
-            { "chief complaint", new List<string> { "What brings you in for an eye examination today?", "Can you describe the main issue or concern you have with your eyes?" } },
-            { "medical history", new List<string> { "Do you have any existing medical conditions?", "Are you currently taking any medications or eye drops?", "Do you have any known allergies to medications or substances?", "Have you ever had eye surgery or any eye-related procedures in the past?" } },
-            { "symptoms", new List<string> { "Could you describe any symptoms you're experiencing, such as pain, redness, or discomfort?", "When did these symptoms start?", "Are the symptoms constant or intermittent?", "Have you noticed any triggers or patterns related to the symptoms?", "Do you have any associated symptoms?" } },
-            { "pain", new List<string> { "Which eye had the pain?", "What is the severity of the pain between 1 to 10, 10 being the most painful?", "How long have you had the pain?", "How would you describe the pain?", "Is there any other issues that you think are related to the pain? e.g. redness?", "Have you had this kind of pain before?", "Each time you had the pain, how long did it last?" } },
-            { "history taking", new List<string> { "Can you describe the patient's chief complaint or reason for the visit?", "Ask about the onset and duration of symptoms.", "Inquire about any relevant medical history or pre-existing conditions.", "Ask about the family history of eye diseases.", "Explore lifestyle factors like smoking, allergies, and occupation.", "Encourage the patient to share any other symptoms or concerns." } }
-            // Add more topics and questions here...
-        };
-
-        public ActionResult AskQuestions()
-        {
-            ViewBag.Topics = questions.Keys;
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult ShowQuestions(string topic)
-        {
-            if (questions.ContainsKey(topic))
-            {
-                ViewBag.Topic = topic;
-                ViewBag.Questions = questions[topic];
-            }
-            else
-            {
-                ViewBag.ErrorMessage = "Invalid topic or questions not found.";
-            }
-
-            return View("AskQuestions");
-        }
-    }
-}
-    */
--- Asst/DAL/QuestionDAL.cs
+++ Asst/DAL/QuestionDAL.cs
@@ -1,5 +1,11 @@
 ﻿using System.Data.SqlClient;
 using Asst.Models;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using Newtonsoft.Json;
+
 namespace Asst.DAL
 {
     public class QuestionDAL
@@ -7,19 +13,56 @@ namespace Asst.DAL
         private IConfiguration Configuration { get; }
         private SqlConnection conn;
 
-        //Constructor
+        // Constructor
         public QuestionDAL()
         {
-            //Read ConnectionString from appsettings.json file
+            // Read ConnectionString from appsettings.json file
             var builder = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json");
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
             Configuration = builder.Build();
-            string strConn = Configuration.GetConnectionString(
-            "OptoAssistConnectionString");
-            //Instantiate a SqlConnection object with the
-            //Connection String read.
+            string strConn = Configuration.GetConnectionString("OptoAssistConnectionString");
+
+            // Instantiate a SqlConnection object with the Connection String read.
             conn = new SqlConnection(strConn);
         }
+
+        public List<QuestionModel> GetQuestions()
+        {
+            List<QuestionModel> questions = new List<QuestionModel>();
+
+            try
+            {
+                conn.Open();
+
+                // Assuming the table structure has 'Topic' and 'question' columns
+                string query = "SELECT Topic, question FROM QuestionTable";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string topic = reader["Topic"].ToString();
+                    string questionsJson = reader["question"].ToString();
+
+                    List<string> questionList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<string>>(questionsJson);
+
+                    QuestionModel questionModel = new QuestionModel
+                    {
+                        Topic = topic,
+                        Questions = questionList
+                    };
+
+                    questions.Add(questionModel);
+                }
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return questions;
+        }
     }
+
 }
