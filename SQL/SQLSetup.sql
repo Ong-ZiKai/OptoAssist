3711c98c9e970fc4a176d49259783c36a55587d2
Zi Kai <ongzikai@gmail.com>
2023-11-16 15:26:02 +0800

    modified sql code (history taking)

--- SQL/SQLSetup.sql
+++ SQL/SQLSetup.sql
@@ -26,7 +26,7 @@ VALUES
     ('chief complaint', '["What brings you in for an eye examination today?", "Can you describe the main issue or concern you have with your eyes?"]'),
     ('medical history', '["Do you have any existing medical conditions?", "Are you currently taking any medications or eye drops?", "Do you have any known allergies to medications or substances?", "Have you ever had eye surgery or any eye-related procedures in the past?"]'),
     ('symptoms', '["Could you describe any symptoms you''re experiencing, such as pain, redness, or discomfort?", "When did these symptoms start?", "Are the symptoms constant or intermittent?", "Have you noticed any triggers or patterns related to the symptoms?", "Do you have any associated symptoms?"]'),
-    ('history taking', '["Can you describe the patient''s chief complaint or reason for the visit?", "Ask about the onset and duration of symptoms.", "Inquire about any relevant medical history or pre-existing conditions.", "Ask about the family history of eye diseases.", "Explore lifestyle factors like smoking, allergies, and occupation.", "Encourage the patient to share any other symptoms or concerns."]'),
+    ('history taking', '["get reason for visit", " get personal and demographic data", " get history of present complain", " get Past ocular history", "get family ocular history", "Encourage the patient to share any other symptoms or concerns."]'),
 	('red eyes','["When did you first notice that your eyes were red, and has the redness been persistent?", "Do you experience any pain or discomfort in addition to the redness?", "Have you used any over-the-counter eye drops or home remedies to address the redness?", "Do you wear contact lenses, and if so, have you noticed any changes in redness since using them?", "Have you been exposed to any potential irritants or had any recent eye injuries?"]'),
 	('pain','["Which eye is affected by the pain you''re experiencing?", "On a scale from 1 to 10, with 10 being the most severe, how would you rate the intensity of the pain?", "How long have you been dealing with this pain?", "Can you describe the nature of the pain, such as sharp, dull, throbbing, or burning?", "Have you noticed any other issues, like redness, associated with the pain?", "Is this the first time you''ve experienced this type of pain, or has it occurred before?", "During each occurrence of the pain, how long does it typically last?"]'),
 	('blurred vision','["When did you first notice changes in your vision, such as blurriness?", "Does the blurred vision occur in one or both eyes?", "Is the blurriness constant, or does it come and go?", "Have you experienced any other visual disturbances, such as flashes of light or floaters?", "Do you wear corrective lenses, and if so, are they up-to-date?"]'),
@@ -44,6 +44,7 @@ VALUES
 INSERT INTO QuestionTableChild(topic, question)
 VALUES
 ('chief Complaint','["When did you first notice any problems with your eyes or vision?","Can you tell me what happened right before your eyes started feeling different?","Can you show me or describe where it hurts or feels uncomfortable around your eyes or head?","How long does the discomfort or vision issue last each time?","Is it constant, or does it come and go?","Can you tell me more about what you''re feeling?","For example, is it like a burning sensation, itching, or maybe like something is in your eye?"]'),
+('history taking', '["get reason for visit", " get personal and demographic data", " get history of present complain", " get Past ocular history", "get family ocular history", "Encourage the patient to share any other symptoms or concerns."]'),
 ('medical History','["Have you noticed anything that makes it feel better?","Does resting your eyes, closing them, or using any specific measures provide relief?","Are there any specific actions or treatments that seem to make the discomfort or vision issues go away temporarily?"]'),
 ('symptoms','["Does the discomfort or vision problem happen at a particular time of day or during specific activities, like reading or playing video games?","On a scale from 1 to 10, with 1 being no discomfort at all and 10 being the most severe discomfort, how would you rate the intensity of your eye or vision issues?"]'),
 ('pain','["Can you remember what you were doing when the pain or discomfort in your eyes started?","Where exactly do you feel the pain?","Can you point to the spot?","How long does the pain last each time?","Can you describe the pain?","Is it sharp, dull, or throbbing?"]'),
@@ -57,3 +58,33 @@ VALUES
 select * from dbo.QuestionTable
 select * from dbo.QuestionTableChild
 
+
+
+--DROP TRIGGER IF EXISTS t_AdultTable_delete;
+--GO
+--CREATE TRIGGER t_AdultTable_delete ON QuestionTable INSTEAD OF DELETE
+--AS BEGIN
+--    DECLARE @topic NVARCHAR(255);
+--    DECLARE @count int;
+--    SELECT @topic = topic FROM deleted;
+--    SELECT @count = COUNT(question) FROM QuestionTable WHERE topic = @topic;
+--    IF @count = 0
+--        DELETE FROM QuestionTable WHERE topic = @topic;
+--    ELSE
+--        THROW 51000, 'can not delete - QuestionTable is referenced in other tables', 1;
+--END;
+
+--DROP TRIGGER IF EXISTS delete_empty_row;
+--GO
+--CREATE TRIGGER delete_empty_row
+--BEFORE DELETE ON QuestionTable
+--FOR EACH ROW
+--BEGIN
+--    IF IS NULL AND NEW.column2 IS NULL AND ... AND NEW.columnN IS NULL THEN
+--        DELETE FROM your_table WHERE 
+--            column1 = OLD.column1 AND
+--            column2 = OLD.column2 AND
+--            ... AND
+--            columnN = OLD.columnN;
+--    END IF;
+--END;
